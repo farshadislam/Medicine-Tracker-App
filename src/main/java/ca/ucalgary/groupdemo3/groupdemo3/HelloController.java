d8d19746f405c82818ec7750247836ea73eca8d7
hiraasad <hira.asad@ucalgary.ca>
2024-04-08 22:24:43 -0600

    fixed size for scroll pane

--- src/main/java/ca/ucalgary/groupdemo3/groupdemo3/HelloController.java
+++ src/main/java/ca/ucalgary/groupdemo3/groupdemo3/HelloController.java
@@ -1,14 +1,161 @@
 package ca.ucalgary.groupdemo3.groupdemo3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button addFoodIntakeButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button addSideEffectsButton;
+
+    @FXML
+    private Button deleteMedicationButton;
+
+    @FXML
+    private Button dosageMGButton;
+
+    @FXML
+    private TextField foodIntakeTextField;
+
+    @FXML
+    private Button highestExpenseButton;
+
+    @FXML
+    private TextField lastPillIntakeTextField;
+
+    @FXML
+    private Label leftInfoLabel;
+
+    @FXML
+    private Button lowestExpenseButton;
+
+    @FXML
+    private TextField madicationNamedelTextField;
+
+    @FXML
+    private TextField medicationFINameTextField;
+
+    @FXML
+    private TextField medicationMGTextField;
+
+    @FXML
+    private TextField medicationSENameTextField;
+
+    @FXML
+    private Button nextPillTimeButton;
+
+    @FXML
+    private TextField pillTimeGapsTextField;
+
+    @FXML
+    private Button refillPeriodButton;
+
+    @FXML
+    private TextField refillPeriodTextField;
+
+    @FXML
+    private Label rightInfoLabel;
+
+    @FXML
+    private TextField sideEffectsTextField;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private Button totalExpenseButton;
+
+    @FXML
+    private Button viewFoodIntakeButton;
+
+    @FXML
+    private Button viewMedicationButton;
+
+    @FXML
+    private Button viewSideEffectsButton;
+
+    @FXML
+    void onAbout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAddFoodIntake(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAddMedicine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAddSideEffects(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClose(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteMedication(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDosageMG(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLoad(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLowestExpense(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNextPillTime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRefillPeriod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTotalExpense(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewFoodIntake(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewMedicationButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewSideEffectsButton(ActionEvent event) {
+
     }
 }
