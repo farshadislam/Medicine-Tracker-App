95ccfefe77266f15ca7c0d20a9828a8329e6ce93
farshad.islam <farshad.islam@ucalgary.ca>
2024-04-11 00:35:25 -0600

    Error handling

--- src/main/java/ca/ucalgary/groupdemo3/groupdemo3/AddController.java
+++ src/main/java/ca/ucalgary/groupdemo3/groupdemo3/AddController.java
@@ -1,7 +1,9 @@
 package ca.ucalgary.groupdemo3.groupdemo3;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class AddController {
@@ -21,6 +23,9 @@ public class AddController {
     @FXML
     private TextField medPrice;
 
+    @FXML
+    private Label addStatus;
+
     private Data data;
 
     private MainController mainController;
@@ -32,11 +37,27 @@ public class AddController {
 
     @FXML
     void addMed(ActionEvent event) {
+        int dose = 0;
+        int fullBottle = 0;
+        int currentBottle = 0;
+        double price = 0;
+
         String name = medName.getText();
-        int dose = Integer.parseInt(medDose.getText());
-        int fullBottle = Integer.parseInt(medFull.getText());
-        int currentBottle = Integer.parseInt(medCurr.getText());
-        double price = Double.parseDouble(medPrice.getText());
+
+        try {
+            dose = Integer.parseInt(medDose.getText());
+            fullBottle = Integer.parseInt(medFull.getText());
+            currentBottle = Integer.parseInt(medCurr.getText());
+        } catch (NumberFormatException e) {
+            addStatus.setText("At least one invalid integer input: " + e.getMessage());
+        }
+
+        try {
+            price = Double.parseDouble(medPrice.getText());
+        } catch (NumberFormatException e) {
+            addStatus.setText("Invalid double input: " + e.getMessage());
+        }
+
         data.storeNewMedicine(name, dose, fullBottle, currentBottle, price);
 
         mainController.viewMedications();
